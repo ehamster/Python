c4ced196a8a253ef96a526b372ac31971d67cc35
YIWU <wy0517@outlook.com>
2016-10-06 14:53:54 -0400

    Create 1.0.py

---
+++ 1.0.py
@@ -0,0 +1,12 @@
+Use // to get a integer number. 5.8//2 = 2;
+Use ** to get power. 2**3 = 8;
+The last printed expression is assigned to the variavle _
+
+String""  ''
+Use \' to express' in String
+Use r'asas\n' to printasas\n.otherwise it would be asas next line
+ print("""\       //\ means end this line;
+ asasas
+ asa
+ """)
+y='a''b'   #y='ab'
