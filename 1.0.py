5fbd3239b285af733bf1eb807cd21afd60bbd825
YIWU <wy0517@outlook.com>
2016-10-06 20:45:29 -0400

    Update 1.0.py

--- 1.0.py
+++ 1.0.py
@@ -1,12 +1,30 @@
-Use // to get a integer number. 5.8//2 = 2;
-Use ** to get power. 2**3 = 8;
-The last printed expression is assigned to the variavle _
+#Use // to get a integer number. 
+5.8//2 = 2;
+#Use ** to get power. 
+2**3 = 8;
+#The last printed expression is assigned to the variavle _
 
 String""  ''
-Use \' to express' in String
-Use r'asas\n' to printasas\n.otherwise it would be asas next line
- print("""\       //\ means end this line;
+#Use \' to express' in String
+#Use r'asas\n' to print asas\n.otherwise it would be asas next line
+ print("""\    \ means end this line;
  asasas
  asa
  """)
 y='a''b'   #y='ab'
+String has subscripted
+a = 'abcde'
+a[2] == c   a[-1]==e  a[-2]==d #-0 is same as 0
+Substring
+a[0:2]  #from 0(included) to 2(excluded)
+a[:2]+a[2:] = a
+length: len(a)
+
+ List
+ String cannot change value, e,g  a[1]='as'; wrong
+ List can change value aa=[1,2,3,4]   aa[1] = 2;
+ #Use append() to add new value   aa.append(12**3);
+  List can be nested  a=[1,2,3]  b=[4,5,6] c=[a,b]
+ c[0][0] = 1;
+ 
+ 
